aea9abf9fa49ee984d7a91b0c594ac9eaa6c6378
Alejandro Martínez Martínez <alejandro93mm@gmail.com>
2022-03-25 18:50:51 +0100

    Created Molecules - Footer

---
+++ src/components/molecules/Form/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+    return (
+        <div className="text-xs font-semibold text-center text-zinc-300 leading-5">
+            <p>Copyright &copy; 2022 Open Bootcamp SL, Imagina Group</p>
+            <p>Todos los derechos reservados.</p>
+            <a
+                className="underline"
+                href="https://open-bootcamp.com/politica-privacidad">
+                Política de Privacidad
+            </a>
+        </div>
+    )
+}
