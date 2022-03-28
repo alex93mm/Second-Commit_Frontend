7eb140060f6a5a6b6dddb91f80cacac68b39fb91
Alejandro Martínez Martínez <alejandro93mm@gmail.com>
2022-03-28 19:24:15 +0200

    Update App Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,47 @@
-import LoginMobile from "./components/templates/Login/LoginMobile"
+import HomePage from "./components/pages/Home/HomePage"
+import LoginPage from "./components/pages/Login/LoginPage"
+import { AuthProvider, useAuth } from "./context/authContext"
+import { Router, Route, useLocation } from 'wouter'
+import RegisterPage from "./components/pages/Login/RegisterPage"
 
 function App() {
+
+  const { token, setToken } = useAuth()
+  const [location, setLocation] = useLocation()
+
+  if (sessionStorage.getItem('token') !== null) {
+    setToken(sessionStorage.getItem('token') || '')
+  } else if (localStorage.getItem('token') !== null) {
+    setToken(localStorage.getItem('token') || '')
+  }
+  
   return (
     <div className="App">
-      <LoginMobile/>
+      <AuthProvider>
+        <Router base="/">
+          <Route path='login'>
+            {token != null
+              ?
+              () => setLocation('/home')
+              :
+              <LoginPage />}
+          </Route>
+          <Route path='register'>
+            {token != null
+              ?
+              () => setLocation('/home')
+              :
+              <RegisterPage />}
+          </Route>
+          <Route path='home'>
+            {token != null
+              ?
+              <HomePage />
+              :
+              () => setLocation('/login')}
+          </Route>
+        </Router>
+      </AuthProvider>
     </div>
   )
 }
