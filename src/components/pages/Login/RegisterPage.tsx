eda930cf04aeb85a7e74eafe93cbf3e47884166e
Alejandro Martínez Martínez <alejandro93mm@gmail.com>
2022-03-28 19:22:10 +0200

    Create Pages - Login, Register, Home

---
+++ src/components/pages/Login/RegisterPage.tsx
@@ -0,0 +1,10 @@
+import RegisterForm from "../../organisms/Login/RegisterForm";
+import LoginTemplate from "../../templates/Login/LoginTemplate";
+
+export default function RegisterPage() {
+    return (
+        <LoginTemplate>
+            <RegisterForm/>
+        </LoginTemplate>
+    )
+}
